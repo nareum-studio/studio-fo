63f63fc18e1252c8c916caaf6cc9c399fa342947
JaeHyeok <re2volution@plateer.com>
2025-12-22 14:01:21 +0900

    footer 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,10 @@
 export default function Footer() {
   return (
-    <footer className=" py-10 mt-10">
-      <div className="container mx-auto">
-        <p className="text-right text-gray-500">
-          나름다움 | 서울 강동구 성안로3길 76 2층 <br />
-          Tel. 010-9095-6639 | KakaoTalk: 나름다움스튜디오
-        </p>
-      </div>
+    <footer className="py-10 px-4">
+      <p className="text-right text-gray-500">
+        나름다움 | 서울 강동구 성안로3길 76 2층 <br />
+        Tel. 010-9095-6639 | KakaoTalk: 나름다움스튜디오
+      </p>
     </footer>
   )
 }
