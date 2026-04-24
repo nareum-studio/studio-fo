45577ebfb4bd53224b517f88355d55cd64c969de
JaeHyeok <re2volution@plateer.com>
2026-04-24 13:11:20 +0900

    이미지 복사 방지

--- components/gallery/UserPhotoGallery.tsx
+++ components/gallery/UserPhotoGallery.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
 import Image from 'next/image'
 
 import { fetchPhotoList } from '@/api/user/fetchPhotoList'
@@ -12,6 +12,9 @@ type UserPhotoGalleryProps = {
 
 export const UserPhotoGallery = ({ category }: UserPhotoGalleryProps) => {
   const [photos, setPhotos] = useState<PhotoItem[]>([])
+  const handlePreventImageCopy = (event: SyntheticEvent) => {
+    event.preventDefault()
+  }
 
   useEffect(() => {
     fetchPhotoList(category).then(setPhotos).catch(console.error)
@@ -20,13 +23,19 @@ export const UserPhotoGallery = ({ category }: UserPhotoGalleryProps) => {
   return (
     <div className="md:grid md:grid-cols-3 flex flex-col gap-4">
       {photos.map((photo) => (
-        <div key={photo.id} className="aspect-2/3 overflow-hidden">
+        <div
+          key={photo.id}
+          className="aspect-2/3 overflow-hidden select-none pointer-events-auto"
+          onContextMenu={handlePreventImageCopy}
+          onDragStart={handlePreventImageCopy}
+        >
           <Image
             src={photo.url}
             alt={photo.originalName}
             width={1000}
             height={1000}
             className="w-full h-full object-cover"
+            draggable={false}
             unoptimized
           />
         </div>
